e877157abba37b12a8b9efdb31d173d9a5e3e7f0
chenhao866 <523888449@qq.com>
2019-04-24 15:08:51 +0800

    Initial commit

---
+++ app/src/main/java/com/smallcheric/picasso/MainActivity.java
@@ -0,0 +1,121 @@
+package com.smallcheric.picasso;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends Activity {
+    public static String[] imageUrls = {
+            "http://i.imgur.com/rFLNqWI.jpg",
+            "http://i.imgur.com/C9pBVt7.jpg",
+            "http://i.imgur.com/rT5vXE1.jpg",
+            "http://i.imgur.com/aIy5R2k.jpg",
+            "http://i.imgur.com/MoJs9pT.jpg",
+            "http://i.imgur.com/S963yEM.jpg",
+            "http://i.imgur.com/rLR2cyc.jpg",
+            "http://i.imgur.com/SEPdUIx.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/76Jfv9b.jpg",
+            "http://i.imgur.com/fUX7EIB.jpg",
+            "http://i.imgur.com/syELajx.jpg",
+            "http://i.imgur.com/COzBnru.jpg",
+            "http://i.imgur.com/Z3QjilA.jpg",
+            "http://i.imgur.com/rFLNqWI.jpg",
+            "http://i.imgur.com/C9pBVt7.jpg",
+            "http://i.imgur.com/rT5vXE1.jpg",
+            "http://i.imgur.com/aIy5R2k.jpg",
+            "http://i.imgur.com/MoJs9pT.jpg",
+            "http://i.imgur.com/S963yEM.jpg",
+            "http://i.imgur.com/rLR2cyc.jpg",
+            "http://i.imgur.com/SEPdUIx.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/76Jfv9b.jpg",
+            "http://i.imgur.com/fUX7EIB.jpg",
+            "http://i.imgur.com/syELajx.jpg",
+            "http://i.imgur.com/COzBnru.jpg",
+            "http://i.imgur.com/Z3QjilA.jpg",
+    };
+    private ListView lv;
+    private GridView gridView;
+    private Button switchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lv);
+        gridView = (GridView) findViewById(R.id.grid);
+        switchButton = (Button) findViewById(R.id.bt);
+        //切换 ListView 与 GridView 的显示模式
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lv.getVisibility()== View.VISIBLE){
+                    lv.setVisibility(View.INVISIBLE);
+                    gridView.setVisibility(View.VISIBLE);
+                }else {
+                    lv.setVisibility(View.VISIBLE);
+                    gridView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        lv.setAdapter(new ImageListAdapter(this,imageUrls));
+        gridView.setAdapter(new ImageListAdapter(this,imageUrls));
+    }
+
+    /**
+     * 适配器
+     */
+    public class ImageListAdapter extends ArrayAdapter{
+        private Context context;
+
+        private String[] imageUrls;
+
+        public ImageListAdapter(Context context,String[] imageUrls){
+            super(context,R.layout.item_picasso,imageUrls);
+
+            this.context = context;
+            this.imageUrls = imageUrls;
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView==null){
+                convertView = View.inflate(context,R.layout.item_picasso,null);
+            }
+            ImageView imageView = (ImageView)convertView;
+            if (TextUtils.isEmpty(imageUrls[position])){
+                Picasso
+                        .with(context)
+                        .cancelRequest(imageView);
+                imageView.setImageDrawable(null);
+            }else {
+                //加载图片
+                Picasso
+                        .with(context)
+                        .load(imageUrls[position])
+                        .placeholder(R.mipmap.ic_launcher)
+                        .error(R.mipmap.ic_launcher)
+                        .noFade()
+                        .into((ImageView) convertView);
+            }
+            return convertView;
+        }
+    }
+}
